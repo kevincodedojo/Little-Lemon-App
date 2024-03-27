11522f8786659373a93482247444d249091b4c3b
“kevin” <ashen7@mail.ccsf.edu>
2024-03-26 17:29:16 -0700

    add OGP semantic tags

--- src/App.js
+++ src/App.js
@@ -3,8 +3,13 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      Heading
-      <h1>Little Lemon</h1>
+      <headr>
+        Heading
+        <h1>Little Lemon</h1>
+      </headr>
+      <nav></nav>
+      <main></main>
+      <footer></footer>
     </div>
   );
 }
